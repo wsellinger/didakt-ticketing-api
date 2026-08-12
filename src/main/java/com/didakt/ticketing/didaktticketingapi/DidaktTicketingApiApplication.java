d2ccae0183296d206ac4cf914fcd3c8a1e3f7431
William Sellinger <wsellinger@gmail.com>
2026-08-12 16:24:25 -0400

    Initial Commit

---
+++ src/main/java/com/didakt/ticketing/didaktticketingapi/DidaktTicketingApiApplication.java
@@ -0,0 +1,13 @@
+package com.didakt.ticketing.didaktticketingapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DidaktTicketingApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DidaktTicketingApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/didakt/ticketing/didaktticketingapi/DidaktTicketingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.didakt.ticketing.didaktticketingapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DidaktTicketingApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
